937fb8701e813ea76a62d4aa41711f46fb850040
Udaybhardwaj179 <103742193+Udaybhardwaj179@users.noreply.github.com>
2022-07-02 03:59:16 +0530

    Add files via upload

---
+++ digital clock.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+using namespace std;
+int main(){
+    int h,m,s;
+    int d=1000;
+    int x=1;
+    cout<<"set time :"<<endl;
+    cin>>h>>m>>s;
+    if (h>12 || m>60 ||s>60)
+    {
+        cout<<"wrong input";
+    }
+    while(1){
+        s++;
+        if  (s>59){
+            m++;
+            s=0;
+        }
+        if(m>59){
+            h++;
+            m=0;
+        }
+        if(h>12){
+            h=1;
+        }
+        cout<<"clock :"<<endl;
+        cout<<h<<" "<<m<<" "<<s;
+        Sleep(d);
+        system("CLS");
+    }
+    return 0;
+}
